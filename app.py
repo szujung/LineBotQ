1cb1f4becfa9d57ab1d6298f963319ce19ffa2a4
szujung <91734880+szujung@users.noreply.github.com>
2022-01-13 16:11:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def index():
                                 "template": {
                                   "type": "buttons",
                                   "title": "Menu",
-                                  "imageBackgroundColor": "#E6C3C3"
+                                  "imageBackgroundColor": "#E6C3C3",
                                   "text": "Please select",
                                   "actions": [
                                       {
